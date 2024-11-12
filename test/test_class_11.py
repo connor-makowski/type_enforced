dd5e91f5deae447f6fc37e1d69ebdcf5054d70ae
Connor Makowski <connorman528@gmail.com>
2024-11-12 13:48:13 -0500

    Add dataclass test

---
+++ test/test_class_11.py
@@ -0,0 +1,31 @@
+import type_enforced
+from dataclasses import dataclass
+
+@type_enforced.Enforcer
+@dataclass
+class Foo:
+    bar: int
+    baz: str
+
+passed = True
+try:
+    foo = Foo(
+        bar=1,
+        baz="a"
+    )
+except:
+    passed = False
+
+try:
+    foo = Foo(
+        bar="a",
+        baz=1
+    )
+    passed = False
+except:
+    pass
+
+if passed:
+    print("test_class_11.py passed")
+else:
+    print("test_class_11.py failed")
