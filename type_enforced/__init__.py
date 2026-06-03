f989a762011fd633079f1803d1af394ed9a8e20a
Connor Makowski <connorman528@gmail.com>
2026-06-03 13:55:35 -0400

    2.5.0: Docs

--- type_enforced/__init__.py
+++ type_enforced/__init__.py
@@ -390,13 +390,13 @@ Contributions are welcome! Please open an issue or submit a pull request.
 
 To avoid extra development overhead, we expect all developers to use a unix based environment (Linux or Mac). If you use Windows, please use WSL2.
 
-For development, we test using Docker so we can lock system deps and swap out python versions easily. However, you can also use a virtual environment if you prefer. We provide a test script and a prettify script to help with development.
+For development, we use [uv](https://docs.astral.sh/uv/) to manage dependencies and run tests.
 
 ## Making Changes
 
 1) Fork the repo and clone it locally.
 2) Make your modifications.
-3) Use Docker or a virtual environment to run tests and make sure they pass.
+3) Run tests and make sure they pass.
 4) Prettify your code.
 5) **DO NOT GENERATE DOCS**.
     - We will generate the docs and update the version number when we are ready to release a new version.
@@ -404,31 +404,16 @@ For development, we test using Docker so we can lock system deps and swap out py
     - Atomic commits are preferred.
 7) Submit a pull request.
 
-## Docker
+## Development Commands
 
-Make sure Docker is installed and running.
+| Command | What it does |
+|---|---|
+| `uv run nox` | Run tests across Python 3.11, 3.12, 3.13, 3.14 |
+| `uv run nox -s tests-3.14` | Run tests on a single Python version |
+| `uv run pytest` | Run tests in the local venv |
+| `uv run pytest -v` | Run tests with verbose output |
+| `uv run python utils/prettify.py` | Format with autoflake + black |
 
-- Create a docker container and drop into a shell
-    - `./run.sh`
-- Run all tests (see ./utils/test.sh)
-    - `./run.sh test`
-- Prettify the code (see ./utils/prettify.sh)
-    - `./run.sh prettify`
-
-- Note: You can and should modify the `Dockerfile` to test different python versions.
-
-## Virtual Environment
-
-- Create a virtual environment
-    - `python3.XX -m venv venv`
-        - Replace `3.XX` with your python version (3.11 or higher)
-- Activate the virtual environment
-    - `source venv/bin/activate`
-- Install the development requirements
-    - `pip install -r requirements/dev.txt`
-- Run Tests
-    - `./utils/test.sh`
-- Prettify Code
-    - `./utils/prettify.sh`"""
-
+Dev dependencies are in `[project.optional-dependencies] dev` in `pyproject.toml`. Install with `uv sync --extra dev`.
+"""
 from .enforcer import Enforcer, FunctionMethodEnforcer
